3c64b1e2524d87709a53692a537d4791125bc7a2
Sean Lee <seancraft79@gmail.com>
2019-03-26 13:13:38 +0900

    Initial commit

---
+++ Docker-MySql/sql-scripts/createtable.sql
@@ -0,0 +1,26 @@
+
+CREATE DATABASE IF NOT EXISTS seandb;
+USE seandb;
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `Uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `UserType` int(1) DEFAULT '1',
+  `UserLevel` int(11) DEFAULT '1',
+  `Email` varchar(256) NOT NULL,
+  `Password` varchar(256) NOT NULL,
+  `CombinedPhoneNumber` varchar(25) DEFAULT NULL,
+  `PostCode` varchar(20) DEFAULT NULL,
+  `Address` varchar(256) DEFAULT NULL,
+  `Country` varchar(128) DEFAULT NULL,
+  `PerchasedItem` varchar(256) DEFAULT NULL,
+  `RegDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `UserName` varchar(256) DEFAULT NULL,
+  `SmsSend` int(1) DEFAULT '1',
+  `EmailSend` int(1) DEFAULT '1',
+  `IsUserValid` int(1) DEFAULT '1',
+  `LastLoginTime` datetime DEFAULT CURRENT_TIMESTAMP,
+  `LanguageCode` varchar(2) DEFAULT 'ko',
+  `DeviceInfo` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`Uid`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
---
+++ SeanDotNetDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeanDotNetDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
